df45fa9151cef089e025d961aeb4ed5716bc9a5e
Francesca Lugay <chescalugay@gmail.com>
2024-06-16 03:21:13 +0800

    Activity 10

---
+++ app/src/main/java/com/example/inventory/MainActivity.kt
@@ -0,0 +1,34 @@
+
+package com.example.inventory
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.inventory.ui.theme.InventoryTheme
+
+//FRANCESCA A. LUGAY
+//BSIT 22A3
+//MOBILE COMPUTING
+//ACTIVITY 10
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+        setContent {
+            InventoryTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    InventoryApp()
+                }
+            }
+        }
+    }
+}
